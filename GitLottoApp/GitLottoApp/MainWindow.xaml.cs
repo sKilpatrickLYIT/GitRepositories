9bddfdcf67581c49099d288f8272ed723f445a4d
sKilpatrickLYIT <L00111786@student.lyit.ie>
2017-02-08 13:46:16 +0000

    oush

--- GitLottoApp/GitLottoApp/MainWindow.xaml.cs
+++ GitLottoApp/GitLottoApp/MainWindow.xaml.cs
@@ -20,14 +20,85 @@ namespace GitLottoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        int maxNumberRange = 0;
+        int[] lottoNumbers = new int[7];
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnIrish_Click(object sender, RoutedEventArgs e)
+        private void btnIrishLotto_Click(object sender, RoutedEventArgs e)
         {
+            maxNumberRange = 52;
+            StartApplication();
+        }
 
+        private void btnUKLotto_Click(object sender, RoutedEventArgs e)
+        {
+            maxNumberRange = 59;
+            StartApplication();
+        }
+
+        private void StartApplication()
+        {
+            int arrayPosition = 0;
+            int randomNumber = 0;
+            lottoNumbers.Initialize();
+
+            do
+            {
+                randomNumber = generateRandomNumber(maxNumberRange);
+                bool exists = checkExisting(randomNumber);
+
+                if (!exists)
+                {
+                    lottoNumbers[arrayPosition] = randomNumber;
+                    arrayPosition++;
+                }
+
+            } while (arrayPosition < lottoNumbers.Length);
+            Array.Sort(lottoNumbers);
+            displayNumbers();
+        }
+
+        private bool checkExisting(int newNumber)
+        {
+            bool exists = false;
+
+            foreach (var number in lottoNumbers)
+            {
+                if (number == newNumber)
+                {
+                    exists = true;
+                }
+            }
+            return exists;
+        }
+
+        private int generateRandomNumber(int maxNumberRange)
+        {
+            int number = 0;
+            Random random1 = new Random();
+            number = random1.Next(1, maxNumberRange);
+            return number;
+        }
+
+        private void displayNumbers()
+        {
+            tbxResult1.Text = lottoNumbers[0].ToString();
+            tbxResult2.Text = lottoNumbers[1].ToString();
+            tbxResult3.Text = lottoNumbers[2].ToString();
+            tbxResult4.Text = lottoNumbers[3].ToString();
+            tbxResult5.Text = lottoNumbers[4].ToString();
+            tbxResult6.Text = lottoNumbers[5].ToString();
+            tbxResultBonus.Text = lottoNumbers[6].ToString();
+        }
+
+        private void bgtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
